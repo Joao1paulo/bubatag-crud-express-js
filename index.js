17af54dae9158e5a665d15b014628f666bbbb620
CristhianHatzman <cristhianhatzman@gmail.com>
2025-04-17 23:46:07 -0300

    feat: adiciona funcionalidade no login com verificação no banco de dados

--- index.js
+++ index.js
@@ -3,14 +3,23 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+import AnimaisControllers from './controllers/AnimaisControllers.js'
+import connection from "./config/sequelize-config.js";
+import LoginController from "./controllers/LoginController.js";
+
+connection.authenticate().then(() => {
+    console.log("Conexão com o banco de dados efetuada!");
+  }).catch((error) => {
+    console.log(error);
+  });
 
 app.get("/", (req, res) => {
   res.render("login");
 });
-
-app.get("/index", (req,res) =>{
-    res.render("index")
-})
+app.use("/", AnimaisControllers);
+app.use("/", LoginController);
 
 app.listen(8086, (error) => {
   if (error) {
